6db146c31a53d935efb2b771981aa8a97bd27c16
GaryChen111 <s111212105@student.nqu.edu.tw>
2026-05-08 10:13:11 +0800

    Create script.js

---
+++ homework/midterm/script.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("gameCanvas");
+
+  // 地板
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 450, 800, 50);
+
+  // 籃板
+  ctx.fillStyle = "black";
+  ctx.fillRect(730, 150, 10, 100);
+
+  // 籃框
+  ctx.fillStyle = "red";
+  ctx.fillRect(hoop.x, hoop.y, hoop.width, hoop.height);
+
+  // 球
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  ctx.fillStyle = "orange";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function gameLoop() {
+
+  if (!gameStarted) return;
+
+  update();
+  draw();
+
+  requestAnimationFrame(gameLoop);
+}
+
+// 排行榜
+
+function saveScore(newScore) {
+
+  let scores = JSON.parse(localStorage.getItem("basketballScores")) || [];
+
+  scores.push(newScore);
+
+  scores.sort((a, b) => b - a);
+
+  scores = scores.slice(0, 5);
+
+  localStorage.setItem("basketballScores", JSON.stringify(scores));
+}
+
+function loadLeaderboard() {
+
+  let scores = JSON.parse(localStorage.getItem("basketballScores")) || [];
+
+  const list = document.getElementById("leaderboardList");
+
+  list.innerHTML = "";
+
+  scores.forEach(score => {
+    const li = document.createElement("li");
+    li.innerText = score + " 分";
+    list.appendChild(li);
+  });
+}
+
+loadLeaderboard();
